1790334a34361188ff566fa003c28ace21fadc44
magegr <magegr@alumnatflorida.es>
2026-04-14 09:10:05 +0200

    añadido login simulado con validación JSON

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,14 +1,30 @@
 import "./login.css";
 import { useNavigate } from "react-router-dom";
 import logo2 from "../../assets/logo2.png";
+import { useState } from "react";
+import data from "../../../database.json";
 
 function Login() {
-
+  const [email , setEmail] = useState("")
+  const [password , setPassword] = useState("")
+  const [mensaje , setMensaje] = useState("")
   const navigate = useNavigate();
-  const handleLogin = () => {
-    navigate("/");
-  };
+  
+   const handleLogin = (e) => {
+    e.preventDefault();
 
+    const datos = data.items.find(
+      (item) =>
+        item.email === email &&
+        item.password.toString() === password
+    );
+
+    if (datos) {
+      navigate("/");
+    } else {
+      setMensaje("Correo o contraseña incorrectos");
+    }
+  }
   return (
       <div className="page">
         <div className="filtro">
@@ -24,12 +40,23 @@ function Login() {
             <form onSubmit={handleLogin} className="login-card">
               <div className="input">
                 <label>Correo</label>
-                <input type="email" placeholder="✉" />
+                <input 
+                    type="email" 
+                    placeholder="✉"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
               </div>
               <div className="input">
                 <label>Contraseña</label>
-                <input type="password" placeholder="🔑" />
+                <input 
+                  type="password" 
+                  placeholder="🔑"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
               </div>
+              <p>{mensaje}</p>
               <button type="submit" className="btn-login">
                 INICIAR SESIÓN
               </button>
